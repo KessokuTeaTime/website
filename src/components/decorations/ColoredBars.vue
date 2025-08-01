2b1a163df800aa5b3e460460be9724a792788c61
KrLite <wang070621@foxmail.com>
2025-08-01 13:22:23 +0800

    ✨ Adorable stylesheets

---
+++ src/components/decorations/ColoredBars.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    v-for="i in ['kita', 'ryo', 'bocchi', 'nijika']"
+    :key="i"
+    :class="`tint-${i}`"
+    class="block"
+  ></div>
+</template>
+
+<style lang="scss">
+.block {
+  flex-grow: 1;
+  background-color: var(--tint);
+}
+</style>
